196d6d208ffa2bafdea079b0e6ad568955f3d41d
Vasiliy Urosov <vasiliy.urosov@phystech.edu>
2014-10-06 07:44:06 +0400

    Malfunctioning page v.2
    Completely rewrote page customers.xhtml. Followed the showcase... Still
    no result...

--- CRM4telecom_stub/CRM4telecom_stub-war/src/java/com/crm4telecom/stub/beans/CustomerManagedBean.java
+++ CRM4telecom_stub/CRM4telecom_stub-war/src/java/com/crm4telecom/stub/beans/CustomerManagedBean.java
@@ -4,11 +4,15 @@ import ejb.jpa.Customer;
 import ejb.beans.CustomerManagerInterface;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.CellEditEvent;
 
-@ManagedBean
-@RequestScoped
+@ManagedBean(name = "cmb")
+@ViewScoped
 public class CustomerManagedBean {
     @EJB
     private CustomerManagerInterface customerManager;
@@ -23,5 +27,14 @@ public class CustomerManagedBean {
     
     public CustomerManagedBean() {
     }
-    
+
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+         
+        if(newValue != null && !newValue.equals(oldValue)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
 }
