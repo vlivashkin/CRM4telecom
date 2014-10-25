8e3c1e97d1e9afa2f80945d7c45cab6b1fd475a3
Anton-Permnenev <anton.permenev@gmail.com>
2014-10-25 23:01:52 +0400

    Balance works as it should

--- CRM4telecom/CRM4telecom-war/src/java/com/crm4telecom/web/beans/CustomerInfoBean.java
+++ CRM4telecom/CRM4telecom-war/src/java/com/crm4telecom/web/beans/CustomerInfoBean.java
@@ -7,6 +7,8 @@ import com.crm4telecom.enums.OrderType;
 import com.crm4telecom.jpa.Customer;
 import com.crm4telecom.jpa.Market;
 import com.crm4telecom.jpa.MarketsCustomers;
+import com.crm4telecom.billing.BillingWebService;
+import com.crm4telecom.billing.Services;
 import com.crm4telecom.jpa.MarketsCustomersPK;
 import com.crm4telecom.jpa.Order;
 import com.crm4telecom.web.util.JSFHelper;
@@ -121,12 +123,11 @@ public class CustomerInfoBean implements Serializable {
     }
 
     public void syncBalance() {
-//        Services service = new Services();
-//        Billing port = service.getBillingPort();
-//        Double result = port.getBalance(customer.getCustomerId());
-//
-//        customer.setBalance(result);
-//        cm.modifyCustomer(customer);
+        Services service = new Services();
+        BillingWebService billingWebService = service.getBillingPort();
+        Double result = billingWebService.getBalance(customer.getCustomerId());
+        cv.setBalance(result);
+
     }
 
     public void updBalance() {
