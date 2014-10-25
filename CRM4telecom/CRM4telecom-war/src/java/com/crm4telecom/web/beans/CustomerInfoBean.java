ee88bf5a4ea062baa6af65cd6db856ee29be379e
VI-yvi-x <ilya.vasilev@phystech.edu>
2014-10-25 23:48:40 +0400

    Bug fixed

--- CRM4telecom/CRM4telecom-war/src/java/com/crm4telecom/web/beans/CustomerInfoBean.java
+++ CRM4telecom/CRM4telecom-war/src/java/com/crm4telecom/web/beans/CustomerInfoBean.java
@@ -89,7 +89,7 @@ public class CustomerInfoBean implements Serializable {
 
         Services service = new Services();
         BillingWebService billingWebService = service.getBillingPort();
-        billingWebService.addCustomer(customerNew.getCustomerId(), cv.getBalance());
+        billingWebService.addCustomer(customerNew.getCustomerId(), cv.getBalance(),customerNew.getStatus().toString());
         
         JSFHelper helper = new JSFHelper();
         helper.redirect("customer_info", "id", customer.getCustomerId().toString());
